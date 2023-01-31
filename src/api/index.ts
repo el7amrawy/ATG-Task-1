ab01ecdb0379d1029bcad85745b2c05aa56e75b5
Aly <el7amrawy@outlook.com>
2023-01-31 19:28:02 +0200

    add mail service

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from "express";
 import usersRoutes from "../handlers/users";
+import resetPasswordRoute from "./reset-Password";
 
 const routes: Router = Router();
 
 routes.use("/users", usersRoutes);
+routes.use("/resetpassword", resetPasswordRoute);
 
 routes.all("*", (_req: Request, res: Response) => {
   res.status(404).send("<h1>404 not found</h1>");
+  //   sendEmail(
+  //     "alihamdyhamdy.27@mail.ru",
+  //     "Reset Password",
+  //     "recovery number is 122343"
+  //   );
 });
 
 export default routes;
---
+++ src/services/mail.ts
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const transportOptions: nodemailer.TransportOptions = {
+  service: "hotmail",
+  auth: {
+    user: process.env.EMAIL_USER as unknown as string,
+    pass: process.env.EMAIL_PASS as unknown as string,
+  },
+};
+
+function sendEmail(reciever: string, subject: string, message: string) {
+  const transporter = nodemailer.createTransport(transportOptions);
+
+  const options = {
+    from: process.env.EMAIL_USER as unknown as string,
+    to: reciever,
+    subject,
+    html: message,
+  };
+  transporter.sendMail(options, (err, info) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+}
+
+export default sendEmail;
