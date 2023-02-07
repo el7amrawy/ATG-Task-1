4d84f46741487f67d3c8ec618a0fb9314c7320c3
Aly <el7amrawy@outlook.com>
2023-02-07 13:56:22 +0200

    add db container

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_URL } = process.env;
+const { DB_URL, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
 
-const client: pg.Pool = new pg.Pool({ connectionString: DB_URL });
+let { ENV } = process.env;
+ENV = ENV?.replace(/\s/g, "");
+
+const dev: pg.ConnectionConfig = {
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+};
+
+const client: pg.Pool = new pg.Pool(
+  ENV === "prod" ? { connectionString: DB_URL } : dev
+);
 
 export default client;
